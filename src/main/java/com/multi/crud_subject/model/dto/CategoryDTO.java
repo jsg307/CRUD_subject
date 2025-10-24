8a3a43b9e9a46f3c65fb0dc4d2f30fad0e9e4af6
Jeon Sung Gu <jsg307@nate.com>
2025-10-24 13:59:50 +0900

    feat: CategoryDTO 데이터입력 DB에서 받아올 DTO정보 입력

--- src/main/java/com/multi/crud_subject/model/dto/CategoryDTO.java
+++ src/main/java/com/multi/crud_subject/model/dto/CategoryDTO.java
@@ -1,4 +1,16 @@
 package com.multi.crud_subject.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CategoryDTO {
+    private int cCode;
+    private String cName;
+
 }
